c981da0cdb150dd9313ca8bb46ec8dca8c8a47ba
Yuri Pereira <yuripereira@cds180-213.cds.ufsc.br>
2013-07-08 18:03:37 -0300

    Testes

--- src/controle/Teste.java
+++ src/controle/Teste.java
@@ -1,167 +1,116 @@
 package controle;
 
+import java.io.File;
+
 public class Teste {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		//TODO Teste semantico 101
+	protected Arquivo arquivo;
+	protected String separador;
 
-		//TODO Teste semantico 102
+	public Teste() {
+		arquivo = new Arquivo();
+		separador = File.separator;
+	}
 
-		//TODO Teste semantico 103
+	public String testes() {
+		StringBuilder erros = new StringBuilder();
+		// TODO Sintatico 109
 
-		//TODO Teste semantico 104
+		// TODO Sintatico 110
 
-		//TODO Teste semantico 105
+		// TODO Sintatico 112
 
-		//TODO Teste semantico 106
+		// TODO Sintatico 113
 
-		//TODO Teste semantico 107
+		// TODO Sintatico 115
 
-		//TODO Teste semantico 108
+		// TODO Sintatico 121
 
-		//TODO Teste semantico 109
+		// TODO Sintatico 122
 
-		//TODO Teste semantico 110
+		// TODO Sintatico 126
 
-		//TODO Teste semantico 111
+		// TODO Sintatico 127
 
-		//TODO Teste semantico 112
+		// TODO Sintatico 129
 
-		//TODO Teste semantico 113
+		// TODO Sintatico 130
 
-		//TODO Teste semantico 114
+		// TODO Sintatico 131
 
-		//TODO Teste semantico 115
+		// TODO Sintatico 132
 
-		//TODO Teste semantico 116
+		// TODO Sintatico 133
 
-		//TODO Teste semantico 117
+		// TODO Sintatico 134
 
-		//TODO Teste semantico 118
+		// TODO Sintatico 135
 
-		//TODO Teste semantico 119
+		// TODO Sintatico 137
 
-		//TODO Teste semantico 120
+		// TODO Sintatico 138
 
-		//TODO Teste semantico 121
+		// TODO Sintatico 139
 
-		//TODO Teste semantico 122
+		// TODO Sintatico 141
 
-		//TODO Teste semantico 123
+		// TODO Sintatico 149
 
-		//TODO Teste semantico 124
+		// TODO Sintatico 150
 
-		//TODO Teste semantico 125
+		// TODO Sintatico 155
 
-		//TODO Teste semantico 126
+		// TODO Sintatico 156
 
-		//TODO Teste semantico 127
+		// TODO Sintatico 161
 
-		//TODO Teste semantico 128
+		// TODO Sintatico 162
 
-		//TODO Teste semantico 129
+		// TODO Sintatico 163
 
-		//TODO Teste semantico 130
+		// TODO Sintatico 164
 
-		//TODO Teste semantico 131
+		// TODO Sintatico 169
 
-		//TODO Teste semantico 132
+		// TODO Sintatico 170
 
-		//TODO Teste semantico 133
+		// TODO Sintatico 171
 
-		//TODO Teste semantico 134
+		// TODO Sintatico 172
 
-		//TODO Teste semantico 135
+		// TODO Sintatico 172
 
-		//TODO Teste semantico 136
+		// TODO Sintatico 178
 
-		//TODO Teste semantico 137
+		// TODO Sintatico 179
 
-		//TODO Teste semantico 138
+		return erros.toString();
+	}
 
-		//TODO Teste semantico 139
+	protected void analisar(String idChecagem, String caminhoArquivo, StringBuilder erros) {
+		boolean erroEsperado;
 
-		//TODO Teste semantico 140
+		if (Character.toUpperCase(caminhoArquivo.charAt(0)) == 'E') {
+			erroEsperado = true;
+		} else {
+			erroEsperado = false;
+		}
 
-		//TODO Teste semantico 141
+		try {
+			Analisador.semantico(abrir(caminhoArquivo));
 
-		//TODO Teste semantico 142
-
-		//TODO Teste semantico 143
-
-		//TODO Teste semantico 144
-
-		//TODO Teste semantico 145
-
-		//TODO Teste semantico 146
-
-		//TODO Teste semantico 147
-
-		//TODO Teste semantico 148
-
-		//TODO Teste semantico 149
-
-		//TODO Teste semantico 150
-
-		//TODO Teste semantico 151
-
-		//TODO Teste semantico 152
-
-		//TODO Teste semantico 153
-
-		//TODO Teste semantico 154
-
-		//TODO Teste semantico 155
-
-		//TODO Teste semantico 156
-
-		//TODO Teste semantico 157
-
-		//TODO Teste semantico 158
-
-		//TODO Teste semantico 159
-
-		//TODO Teste semantico 160
-
-		//TODO Teste semantico 161
-
-		//TODO Teste semantico 162
-
-		//TODO Teste semantico 163
-
-		//TODO Teste semantico 164
-
-		//TODO Teste semantico 165
-
-		//TODO Teste semantico 166
-
-		//TODO Teste semantico 167
-
-		//TODO Teste semantico 168
-
-		//TODO Teste semantico 169
-
-		//TODO Teste semantico 170
-
-		//TODO Teste semantico 171
-
-		//TODO Teste semantico 172
-
-		//TODO Teste semantico 173
-
-		//TODO Teste semantico 174
-
-		//TODO Teste semantico 175
-
-		//TODO Teste semantico 176
-
-		//TODO Teste semantico 177
-
-		//TODO Teste semantico 178
-
+			if (erroEsperado) {
+				erros.append(idChecagem + " - Esperava-se erro \n");
+			}
+		} catch (Exception e) {
+			if (!erroEsperado) {
+				erros.append(idChecagem + " - Erro não esperado - " + e.getMessage() + "\n");
+			}
+		}
+	}
+
+	protected String abrir(String nomeArquivo) {
+		return arquivo.abrir("testes" + separador + nomeArquivo + ".txt");
 	}
 
 }
